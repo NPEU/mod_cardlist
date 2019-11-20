d3d0082c8ff26c1fd46395db6a774d538a64f979
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-11-20 14:33:04 +0000

    First go

--- tmpl/default.php
+++ tmpl/default.php
@@ -8,15 +8,56 @@
  */
 
 defined('_JEXEC') or die;
+use Joomla\String\StringHelper;
 
 
 $doc = JFactory::getDocument();
+$hx = StringHelper::increment($params->get('header_tag'));
+?>
+<?php if ($module->showtitle): ?>
+<<?php echo $params->get('header_tag'); ?>><?php echo $module->title; ?></<?php echo $params->get('header_tag'); ?>>
+<?php endif; ?>
+
+<?php if (!empty($params->get('cards'))) : ?>
+<div>
+<?php foreach ($params->get('cards') as $card) : 
+
+if (!empty($card->link && !empty($card->link_text))) {
+    $card->body .= '<p><a href="' . $card->link .'">' . $card->link_text . '</a></p>';
+}
+
+$full_link = false;
+
+if (!empty($card->link) && (bool) $params->get('link_full')) {
+    $full_link = true;
+}
 
 ?>
+    <div>
+    <?php echo $full_link ? '<a href="' . $card->link .'">' : ''; ?>
 
-<?php if ($params->get('show_title')): ?>
-<p><?php echo $params->get('title'); ?></p>
-<?php endif; ?>
-<?php if ($params->get('thing')): ?>
-<p><?php echo $params->get('thing'); ?></p>
+        <?php if (!empty($card->header_image)) : ?>
+        <img src="<?php echo $card->header_image; ?>" alt="<?php echo $card->header_image_alt; ?>">
+        <?php endif; ?>
+
+        <<?php echo $hx; ?>><?php echo $card->title; ?></<?php echo $hx; ?>>
+
+        <?php if (!empty($card->body)) : ?>
+        <div>
+            <?php echo $card->body; ?>
+        </div>
+        <?php endif; ?>
+
+        <?php if (!empty($card->footer)) : ?>
+        <?php echo $card->footer; ?>
+        <?php endif; ?>
+
+        <?php if (!empty($card->footer_image)) : ?>
+        <img src="<?php echo $card->footer_image; ?>" alt="<?php echo $card->footer_image_alt; ?>">
+        <?php endif; ?>
+
+    <?php echo $full_link ? '</a>' : ''; ?>
+    </div>
+<?php endforeach; ?>
+</div>
 <?php endif; ?>
